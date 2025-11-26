2da8fd75f1031b58295209fd826545783bff01d4
Houstonzaihubei <Houstonzaihubei@163.com>
2025-11-26 23:39:09 +0800

    Record for LeetCode

---
+++ LeetCode/1.two-sum.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author: OBKoro1
+ * @Date: 2025-11-26 23:34:37
+ * @FilePath: \LeetCode\1.two-sum.cpp
+ * @Description: 
+ */
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
+
